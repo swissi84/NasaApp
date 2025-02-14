054111ab9db56521daeee44df434926417a0d8bb
Andre Eggenschwiler <swissi1984@gmail.com>
2025-02-14 16:27:50 +0100

    Friday  bugfix

--- app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/MainActivity.kt
+++ app/src/main/java/de/syntax_institut/jetpack/a04_05_online_shopper/MainActivity.kt
@@ -1,13 +1,16 @@
 package de.syntax_institut.jetpack.a04_05_online_shopper
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import de.syntax_institut.jetpack.a04_05_online_shopper.Navigation.AppNavigation
 
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
